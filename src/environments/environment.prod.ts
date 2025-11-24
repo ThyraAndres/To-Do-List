54c63d61c4e3b3132816c67a9b25cf470a2b218d
William <thyraelnegra@gmail.com>
2025-11-24 13:24:52 -0500

    APK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "your-api-key",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "your-app-id"
+    apiKey: "AIzaSyBJ6Ubt2MuH5UMZ4AYvlcX6DU-2hwwgRdI",
+    authDomain: "todolist-demo-638d2.firebaseapp.com",
+    projectId: "todolist-demo-638d2",
+    storageBucket: "todolist-demo-638d2.firebasestorage.app",
+    messagingSenderId: "719161394166",
+    appId: "1:719161394166:web:afd9e85b5e62b4c0553dd7",
+    measurementId: "G-RQZW1LF9JH"
   }
 };
